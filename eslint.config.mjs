a6e4b506a82d194e2d09abd3b273887221a23159
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2025-09-17 16:12:13 -0500

    Add ESLint and Prettier configuration and tooling

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["worker-configuration.d.ts"],
+  },
+);
